31a568af7db27d7c50f16d6a2e3d60fd4f85e3a8
jgranadosc <jpgc.edu@gmail.com>
2024-03-15 19:44:45 -0600

    Master commit

---
+++ src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/MainRunExamples.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.paraiso.ie.algoritmos;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainRunExamples 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
